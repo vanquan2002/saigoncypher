5dbcc4e484c6f5d4383a2518f4544084a8c9ecae
Pham Van Quan <quan100202@gmail.com>
2024-07-17 17:19:16 +0700

    update

--- BE/index.js
+++ BE/index.js
@@ -15,8 +15,8 @@ app.use("/api/products", productRoute);
 app.use("/api/users", userRoute);
 app.use("/api/orders", orderRoute);
 
-// app.use(notFound);
-// app.use(errorHandler);
+app.use(notFound);
+app.use(errorHandler);
 
 app.get("/", (req, res) => res.send("API is running..."));
 
