742dfc1bf0df39d17fa20898e2a3d62e141f360e
Pham Van Quan <quan100202@gmail.com>
2024-07-17 18:46:37 +0700

    update

--- BE/config/Mongoose.js
+++ BE/config/Mongoose.js
@@ -3,9 +3,6 @@ import mongoose from "mongoose";
 const connectDatabase = async () => {
   try {
     const uri = process.env.MONGO_URL;
-    if (!uri) {
-      throw new Error("MONGODB_URL environment variable is not defined");
-    }
     await mongoose.connect(uri, {
       useUnifiedTopology: true,
       useNewUrlParser: true,
